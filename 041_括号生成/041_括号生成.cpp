80d9297e172241c66745ba9cd94938bad231cfd4
钰 刘 <18391128562@163.com>
2026-02-03 16:50:20 +0800

    括号问题总结: 使用回溯生成括号，在生成过程中保证,左括号不超过 n，右括号不超过左括号

---
+++ 041_括号生成/041_括号生成.cpp
@@ -0,0 +1,42 @@
+﻿#include <iostream>
+#include <vector>
+#include <string>
+using namespace std;
+
+class Solution {
+public:
+    vector<string> result;
+    //回朔信息
+    void backtrack(string current, int open, int close, int n) {
+        if (open == n && close == n) {// 如果已经用完 n 对括号，加入结果
+            result.push_back(current);
+            return;
+        }
+
+        // 如果还能加 '('
+        if (open < n) {
+            backtrack(current + "(", open + 1, close, n);
+        }
+
+        // 如果还能加 ')'（不能超过 '(' 的数量）
+        if (close < open) {
+            backtrack(current + ")", open, close + 1, n);
+        }
+    }
+
+    vector<string> generateParenthesis(int n) {
+        backtrack("", 0, 0, n);
+        return result;
+    }
+};
+
+int main(){
+    Solution s;
+    int n = 3;
+    vector<string> res = s.generateParenthesis(n);
+    for (auto& str : res) {
+        cout << str << endl;
+    }
+    return 0;
+}
+
