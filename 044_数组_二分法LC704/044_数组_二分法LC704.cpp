02ad3722ecf2a2f86ae2e182f96b390ef1a1c157
钰 刘 <18391128562@163.com>
2026-03-09 21:38:23 +0800

    力扣二分法实现

---
+++ 044_数组_二分法LC704/044_数组_二分法LC704.cpp
@@ -0,0 +1,32 @@
+﻿
+#include <iostream>
+#include <vector>
+using namespace std;
+
+class Solution {
+public:
+    int search(vector<int>& nums, int target) {
+        int left = 0;
+        int right = nums.size() - 1;
+        int middle = 0;
+        while (left <= right) {
+            middle = (left + right) / 2;
+            if (nums[middle] > target) {
+                right = middle - 1;
+            }else if (nums[middle] < target) {
+                left = middle + 1; 
+            }else {
+                return middle;
+            }
+        }
+        return -1;
+    }
+};
+
+int main(){
+    vector<int> nums = { 1,2,3,4,5,6,7,8,9,10,12 };
+    Solution solution;
+    int result = solution.search(nums, 7);
+    cout << "result=" << result;
+}
+
