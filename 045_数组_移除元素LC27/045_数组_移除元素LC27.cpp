240609ec187c4a2b2c5bffde863c86f5221d4385
钰 刘 <18391128562@163.com>
2026-03-10 21:37:20 +0800

    移除数组元素问题(快慢指针) 注意快指针的边界问题

---
+++ 045_数组_移除元素LC27/045_数组_移除元素LC27.cpp
@@ -0,0 +1,25 @@
+﻿#include <iostream>
+#include <vector>
+using namespace std;
+
+class Solution {
+public:
+    int removeElement(vector<int>& nums, int val) {
+        int slow = 0;
+        for (int fast = 0; fast < nums.size();fast++) {//注意fast的边界问题
+            if (nums[fast] != val) {
+                nums[slow] = nums[fast];
+                slow++;
+            }
+        }
+        return slow;
+    }
+};
+
+int main()
+{
+    vector<int> nums = { 1,2,3,4,5,7,7,7,7,7,12 };
+    Solution solution;
+    int result = solution.removeElement(nums, 7);
+    cout << "result=" << result;
+}
