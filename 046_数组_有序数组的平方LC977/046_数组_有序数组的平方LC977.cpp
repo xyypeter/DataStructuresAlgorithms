a60a58535959c30d846841fd26e5f8261904e8a6
钰 刘 <18391128562@163.com>
2026-03-17 17:11:03 +0800

    数组双指针题型 注意的是:因为原数组可能包含负数，平方后最大值可能在数组两端 使用两个指针 i和 j分别指向数组的开头和结尾

---
+++ 046_数组_有序数组的平方LC977/046_数组_有序数组的平方LC977.cpp
@@ -0,0 +1,33 @@
+﻿#include <iostream>
+#include <vector>
+using namespace std;
+
+class Solution {
+public:
+    vector<int> sortedSquares(vector<int>& nums) {
+        int k = nums.size() - 1;
+        vector<int> result(nums.size());
+        for (int i = 0, j = nums.size() - 1; i <= j; ) {
+            if ((nums[i] * nums[i]) > (nums[j] * nums[j])) {
+                result[k--] = nums[i] * nums[i];
+                i++;
+            }else {
+                result[k--] = nums[j] * nums[j];
+                j--;
+            }
+        }
+        return result;
+    }
+};
+
+int main(){
+    vector<int> nums = { -4,-1,0,3,10 };
+    Solution solution;
+    vector<int> result = solution.sortedSquares(nums);
+    for (auto value : result) {
+        cout << value << " " ;
+    }
+    cout << endl;
+}
+
+
