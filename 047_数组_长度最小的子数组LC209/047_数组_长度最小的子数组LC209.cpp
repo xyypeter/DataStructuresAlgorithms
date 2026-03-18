16d06734088b315f12b5682c94a470f5a71c5377
钰 刘 <18391128562@163.com>
2026-03-18 09:44:35 +0800

    长度最小的子数组 算法核心思路:通过双指针维护一个可变窗口,在数组上滑动寻找满足条件的最小连续子数组长度 需要注意的点是:数组元素均为正数（确保滑动窗口的单调性）

---
+++ 047_数组_长度最小的子数组LC209/047_数组_长度最小的子数组LC209.cpp
@@ -0,0 +1,32 @@
+﻿#include <iostream>
+#include <vector>
+using namespace std;
+
+class Solution {
+public:
+    int minSubArrayLen(int target, vector<int>& nums) {
+        int n = nums.size();
+        if (n == 0) return 0;
+        int ans = INT_MAX;     //记录最小长度
+        int start = 0, end = 0;//窗口的左右边界指针
+        int sum = 0;           //当前窗口内的和
+        while (end < n) {
+            sum += nums[end];
+            while (sum >= target) {
+                ans = min(ans, end - start + 1);
+                sum -= nums[start];               //关键步骤-调整窗口内的元素和
+                start++;
+            }
+            end++;
+        }
+        return ans == INT_MAX ? 0 : ans;
+    }
+};
+
+int main(){
+    vector<int> nums = { 2,3,1,2,4,3 };
+    Solution solution;
+    int result = solution.minSubArrayLen(7,nums);
+    cout << result << endl;
+}
+
