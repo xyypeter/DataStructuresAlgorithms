76ae42627e8c6754cb6ef2bc0a11a7e30c74ad8a
钰 刘 <18391128562@163.com>
2026-03-25 11:37:52 +0800

    反转链表 使用双指针实现链表的反转

---
+++ 051_链表_反转链表_LC206/051_链表_反转链表_LC206.cpp
@@ -0,0 +1,58 @@
+﻿#include <iostream>
+using namespace std;
+
+struct ListNode {
+    int val;
+    ListNode* next;
+    ListNode() :val(0), next(nullptr) {}
+    ListNode(int x) :val(x), next(nullptr) {}
+    ListNode(int x, ListNode* next) :val(x), next(next) {}
+};
+
+class Solution {
+public:
+    ListNode* reverseList(ListNode* head) {
+        ListNode* cur = head;
+        ListNode* pre = nullptr;
+        ListNode* tmp = nullptr;
+        while (cur) {
+            tmp = cur->next;
+            cur->next = pre;
+            pre = cur;
+            cur = tmp;
+        }
+        return pre;
+    }
+};
+
+// 打印链表
+void printList(ListNode* head) {
+    ListNode* curr = head;
+    while (curr != nullptr) {
+        cout << curr->val;
+        if (curr->next != nullptr) cout << " -> ";
+        curr = curr->next;
+    }
+    cout << endl;
+}
+
+int main() {
+    ListNode* list = new ListNode(1);
+    list->next = new ListNode(2);
+    list->next->next = new ListNode(3);
+    list->next->next->next = new ListNode(4);
+    list->next->next->next->next = new ListNode(5);
+    list->next->next->next->next->next = new ListNode(6);
+    list->next->next->next->next->next->next = new ListNode(7);
+    printList(list);
+
+    Solution s;
+    ListNode* result = s.reverseList(list);
+
+    // 打印结果
+    cout << "Result: ";
+    printList(result);
+
+    return 0;
+}
+
