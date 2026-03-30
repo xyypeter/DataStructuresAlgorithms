20dcf9319cb6db31fc7f0dd91b6c93f1e497c9ab
钰 刘 <18391128562@163.com>
2026-03-30 17:07:56 +0800

    双指针的经典应用

---
+++ 053_链表_删除链表倒数第N个节点_LC19/053_链表_删除链表倒数第N个节点_LC19.cpp
@@ -0,0 +1,62 @@
+﻿#include <iostream>
+using namespace std;
+
+struct ListNode {
+    int val;
+    ListNode* next;
+    ListNode() :val(0), next(nullptr) {}
+    ListNode(int x) :val(x), next(nullptr) {}
+    ListNode(int x, ListNode* next) :val(x), next(next) {}
+};
+
+class Solution {
+public:
+    ListNode* removeNthFromEnd(ListNode* head, int n) {
+        ListNode* dummyHead = new ListNode(0);
+        dummyHead->next = head;
+        ListNode* slow = dummyHead;
+        ListNode* fast = dummyHead;
+        while (n-- && fast != nullptr) {
+            fast = fast->next;
+        }
+        fast = fast->next;
+        while (fast != nullptr) {
+            fast = fast->next;
+            slow = slow->next;
+        }
+        ListNode *tmp = slow->next; 
+        slow->next = tmp->next;
+        delete tmp;
+        return dummyHead->next;
+    }
+};
+
+// 打印链表
+void printList(ListNode* head) {
+    ListNode* curr = head;
+    while (curr != nullptr) {
+        cout << curr->val;
+        if (curr->next != nullptr) cout << " -> ";
+        curr = curr->next;
+    }
+    cout << endl;
+}
+
+int main() {
+    ListNode* list = new ListNode(1);
+    list->next = new ListNode(2);
+    list->next->next = new ListNode(3);
+    list->next->next->next = new ListNode(4);
+    list->next->next->next->next = new ListNode(5);
+    printList(list);
+
+    Solution s;
+    ListNode* result = s.removeNthFromEnd(list,2);
+
+    // 打印结果
+    cout << "Result: ";
+    printList(result);
+
+    return 0;
+}
+
