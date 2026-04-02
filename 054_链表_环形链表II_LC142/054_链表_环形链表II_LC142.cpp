fa2c9327f28b9a0eb63b51c33b753b3392114445
钰 刘 <18391128562@163.com>
2026-04-02 16:53:02 +0800

    环形链表II 注意:判断环入口时，一定注意while而不是if

---
+++ 054_链表_环形链表II_LC142/054_链表_环形链表II_LC142.cpp
@@ -0,0 +1,61 @@
+﻿#include <iostream>
+using namespace std;
+
+struct ListNode {
+    int val;
+    ListNode* next;
+    ListNode() :val(0), next(nullptr) {}
+    ListNode(int x) :val(x), next(nullptr) {}
+    ListNode(int x, ListNode* next) :val(x), next(next) {}
+};
+
+class Solution {
+public:
+    ListNode* detectCycle(ListNode* head) {
+        ListNode* fast = head;
+        ListNode* slow = head;
+        while (fast != nullptr && fast->next != nullptr) {
+            slow = slow->next;
+            fast = fast->next->next;//三个才能成环，所以快指针移动两步
+            if (slow == fast) {
+                ListNode* index1 = fast;
+                ListNode* index2 = head;
+                while (index1 != index2) {
+                    index1 = index1->next;
+                    index2 = index2->next;
+                }
+                return index2;
+            }
+        }
+        return nullptr;
+    }
+};
+
+int main() {
+    Solution sol;
+
+
+    ListNode* n1 = new ListNode(1);
+    ListNode* n2 = new ListNode(2);
+    ListNode* n3 = new ListNode(3);
+    ListNode* n4 = new ListNode(4);
+    ListNode* n5 = new ListNode(5);
+
+    n1->next = n2;
+    n2->next = n3;
+    n3->next = n4;
+    n4->next = n5;
+    n5->next = n3; // 环从3开始
+
+    ListNode* res = sol.detectCycle(n1);
+    if (res)
+        cout << "Test2: Cycle at " << res->val << endl;
+    else
+        cout << "Test2: No cycle" << endl;
+
+
+    return 0;
+}
+
+
+
