c06b087c988044fad0ea33ed7977e198c0d553e8
钰 刘 <18391128562@163.com>
2026-04-14 17:32:25 +0800

    反转字符串单词 1.移除多余空格 2.将整个字符串反转 3.将每个单词反转

---
+++ 063_字符串_反转字符串中的单词/063_字符串_反转字符串中的单词.cpp
@@ -0,0 +1,38 @@
+﻿
+#include <iostream>
+#include <string>
+using namespace std;
+
+class Solution {
+public:
+    string reverseWords(string s) {
+        s.push_back(' ');
+        int n = s.size(), i = 0;
+        for (int j = 0; j < n;)
+        {
+            if (s[j] != ' ')
+            {
+                int idx = i;
+                while (j < n && s[j] != ' ') s[i++] = s[j++];
+                reverse(s.begin() + idx, s.begin() + i);
+                s[i++] = ' ';
+            }
+            ++j;
+        }
+        s.resize(i - 1);
+        reverse(s.begin(), s.end());
+        return s;
+    }
+};
+
+
+
+int main() {
+    Solution solution;
+
+    string s = "the sky is blue";
+    string result = solution.reverseWords(s);
+
+    cout << result << endl;
+
+}
